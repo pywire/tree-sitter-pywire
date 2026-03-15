d570764cb31bf27a0e9e5a4af3d6c7493cbab1c8
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2026-03-14 18:10:42 -0600

    feat: update grammer for highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,17 +1,41 @@
+; === Separators ===
+(separator) @punctuation.delimiter
+
+; === Directives ===
+(directive) @keyword.directive
+(block_directive) @keyword.directive
+
+; === Tags ===
 (tag_name) @tag
+(doctype) @tag
+
+; === Attributes ===
 (attribute_name) @attribute
-(special_attribute_name) @constructor
-(directive) @keyword
-(block_directive) @keyword
+(special_attribute_name) @attribute.special
+(attribute_value) @string
+
+; === Template control blocks ===
+(brace_block "{$" @keyword.control)
+(brace_block "}" @keyword.control)
+(brace_block keyword: (_) @keyword.control)
+
+(end_brace_block "{/" @keyword.control)
+(end_brace_block "}" @keyword.control)
+(end_brace_block name: (_) @keyword.control)
+
+; === Interpolation braces ===
+(interpolation "{" @punctuation.bracket)
+(interpolation "}" @punctuation.bracket)
+
+; === Attribute shorthand braces ===
+(attribute_shorthand "{" @punctuation.bracket)
+(attribute_shorthand "}" @punctuation.bracket)
+
+; === Spread shorthand ===
+(spread_shorthand "{" @punctuation.bracket)
+(spread_shorthand "**" @operator)
+(spread_shorthand "}" @punctuation.bracket)
+
+; === Comments ===
 (comment) @comment
-
-(brace_block "{$" @keyword)
-(brace_block "}" @keyword)
-(brace_block keyword: (_) @keyword)
-
-(end_brace_block "{/" @keyword)
-(end_brace_block "}" @keyword)
-(end_brace_block name: (_) @keyword)
-
-(python_code) @embedded.python
 
--- queries/injections.scm
+++ queries/injections.scm
@@ -1,2 +1,12 @@
+; Inject Python into the frontmatter block (between the --- delimiters)
+((python_content) @injection.content
+ (#set! injection.language "python"))
+
+; Inject Python into inline expressions: { expr }, {$ for expr }, {** expr }
 ((python_code) @injection.content
-  (#set! injection.language "python"))
+ (#set! injection.language "python"))
+
+; Inject HTML into the template section (everything after the frontmatter)
+((template_section) @injection.content
+ (#set! injection.language "html"))
+
